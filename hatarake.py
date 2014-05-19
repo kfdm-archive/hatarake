2089f614f01f84c74741bdef641be9e2b97d9d7c
Paul Traylor <kungfudiscomonkey@gmail.com>
2014-05-18 19:58:47 -0700

    Support for reloading status from database.

--- hatarake.py
+++ hatarake.py
@@ -9,7 +9,7 @@ import rumps
 
 from gntp.config import GrowlNotifier
 
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.WARNING)
 
 NSTIMEINTERVAL = 978307200
 
@@ -29,18 +29,11 @@ GROWL_INTERVAL = 30
 class Hatarake(rumps.App):
     def __init__(self):
         super(Hatarake, self).__init__("Hatarake")
-        self.menu = ["Duration"]
-        self.label = self.menu["Duration"]
+        self.menu = ["Reload", "Debug"]
+        self.label = self.menu["Reload"]
+        self.delay = GROWL_INTERVAL
 
-        with sqlite3.connect(POMODORO_DB) as con:
-            cur = con.cursor()
-            cur.execute(POMODORO_SQL)
-            zwhen, zname = cur.fetchone()
-
-        self.zname = zname
-        self.when = datetime.datetime.fromtimestamp(
-            zwhen + NSTIMEINTERVAL, pytz.utc
-        ).replace(microsecond=0)
+        self.reload(None)
 
         self.notifier = GrowlNotifier(
             applicationName='Hatarake',
@@ -64,8 +57,33 @@ class Hatarake(rumps.App):
         self.label.title = u'Last pomodoro [{0}] was {1} ago'.format(self.zname, delta)
         self.title = u'働け {0}'.format(delta)
 
-        if delta.total_seconds() % GROWL_INTERVAL == 0:
+        if delta.total_seconds() % self.delay == 0:
             self.alert(u'[{0}] was {1} ago', self.zname, delta)
+
+    @rumps.clicked("Reload")
+    def reload(self, sender):
+        logging.info('Reloading db')
+        with sqlite3.connect(POMODORO_DB) as con:
+            cur = con.cursor()
+            cur.execute(POMODORO_SQL)
+            zwhen, zname = cur.fetchone()
+
+        self.zname = zname
+        self.when = datetime.datetime.fromtimestamp(
+            zwhen + NSTIMEINTERVAL, pytz.utc
+        ).replace(microsecond=0)
+
+    @rumps.clicked("Debug")
+    def toggledebug(self, sender):
+        sender.state = not sender.state
+        if sender.state:
+            self.delay = 5
+            logging.getLogger().setLevel(logging.INFO)
+            logging.info('Setting debugging to INFO and delay to %d', self.delay)
+        else:
+            logging.info('Setting debugging to WARNING and delay to %d', self.delay)
+            logging.getLogger().setLevel(logging.WARNING)
+            self.delay = GROWL_INTERVAL
 
 if __name__ == "__main__":
     Hatarake().run()
