54bb4f2d885fe0d672d4d320410b56a276de71f5
Paul Traylor <kungfudiscomonkey@gmail.com>
2014-10-26 21:14:02 -0700

    Quiet some of the growl logging

--- hatarake/__main__.py
+++ hatarake/__main__.py
@@ -3,4 +3,5 @@ from hatarake.cli import Hatarake
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('gntp').setLevel(logging.INFO)
     Hatarake().run()
